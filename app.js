47d1e3f17f4c4d120b5ffa0c69cf6bc2443b5f30
lillielove <lillielove@gmail.com>
2022-07-02 12:11:54 -0700

    add inquirer package, first prompt and promise test

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const fs = require('fs');
-const generatePage = require("./src/page-template.js");
+const inquirer=require('inquirer');
 
-const profileDataArgs = process.argv.slice(2); //, process.argv.length
+// const fs = require('fs');
+// const generatePage = require("./src/page-template.js");
 
-const [yourName, github] = profileDataArgs;
+// // const profileDataArgs = process.argv.slice(2); //, process.argv.length
 
-fs.writeFile("./index.html", generatePage(yourName, github), err=>{
-    if(err) throw new Error(err);
+// // const [yourName, github] = profileDataArgs;
+// const pageHTML = generatePage(aName, github);
 
-    console.log("Portfolio complete!");
-});
+// fs.writeFile("./index.html", pageHTML, err=>{
+//     if(err) throw err;
+
+//     console.log("Portfolio complete!");
+// });
+
+inquirer
+    .prompt([
+        {
+            type:"input",
+            name:"name",
+            message:"What's your name?"
+    
+        }
+    ])
+    .then(answers => console.log(answers));
+
 
